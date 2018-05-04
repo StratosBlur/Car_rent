b27c145551f7736c38d7252bddada5720ee002a1
WaveBlur <wave_onlyone@hotmail.com>
2018-05-04 16:50:17 +0700

    pull

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Basic Car Rental System
+เป็นโครงงานในรายวิชา Software Engineer และ Internet Programing
---
+++ car_rent/src/components/NotFound.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+
+class NotFound extends Component {
+    render(){
+        return (
+            <div>
+                <br />
+                <h1>ERROR 404 Not Found</h1>
+                <hr />
+            </div>
+        )
+    }
+}
+
+export default NotFound
